fdf988fe6c20dbd8c9d2f7ff99f7da6635c017de
ASChavis <suzycodes@gmail.com>
2023-09-15 13:00:57 -0400

    tinkering with Express
    Starting to play with Postman

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-const pokeURL = "https://pokeapi.co/api/v2/pokemon/";
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/submit", (req, res) => {
+    console.log(req.body);
+});
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello!</h1>");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`)
+});
+
+/*const pokeURL = "https://pokeapi.co/api/v2/pokemon/";
 const searchBtn = document.querySelector(".pokemonSearch");
 const searchBar = document.querySelector("input");
 
@@ -8,7 +27,7 @@ async function whosThatPokemon(pokemon) {
     var data = await response.json();
 
     console.log(data);
-/*
+
     const pokeArray = {
         ".slot1" : data.results.,
         ".slot2", 
@@ -24,7 +43,7 @@ async function whosThatPokemon(pokemon) {
     for(let className in pokeArray) {
         document.querySelector(className).innerHTML = pokeArray[className];
     }
-   */ 
+   
 
 };
 
@@ -38,3 +57,5 @@ searchBar.addEventListener("keydown", function(event){
         whosThatPokemon(searchBar.value);
     }
 });
+
+*/
