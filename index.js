ec555032fea045eca14e60a075f1e4d964f02683
ASChavis <suzycodes@gmail.com>
2023-09-26 08:16:33 -0400

    linking column 1 to api

--- index.js
+++ index.js
@@ -1,14 +1,66 @@
-import express from "express";
 
-const app = express();
-const port = 3000;
-
-app.use(express.json()); // For parsing application/json
-app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+const searchBtn = document.querySelector(".pokemonSearch");
+const searchBar = document.querySelector("input");
 
 
+async function displayPokemon(pokemonName){
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}.`)
+    const pokeURL ="https://pokeapi.co/api/v2/pokemon/";
+
+    const response = await fetch(pokeURL + pokemonName);
+    const data = await response.json();
+
+    console.log(data);
+
+
+const pokemonCard = {
+    ".pokemonName": data.name,
+    ".pokemonType": "Type: " + data.types[0].type.name,
+    ".height": "Height: " + data.height + " ft",
+    ".weight": "Weight: " + data.weight + " lbs",
+
+};
+
+for (let className in pokemonCard) {
+    document.querySelector(className).innerHTML = pokemonCard[className];
+}
+
+};
+
+/*
+async function whosThatPokemon(pokemon) {
+    const pokeURL = "https://pokeapi.co/api/v2/pokemon/";
+    const response = await fetch(pokeURL + searchBar.innerHTML);
+    var data = await response.json();
+
+    console.log(data);
+
+    const pokeArray = {
+        ".slot1" : data.results.name,
+        ".slot2" : data.results.name, 
+        ".slot3" : data.results.name, 
+        ".slot4" : data.results.name, 
+        ".slot5" : data.results.name, 
+        ".slot6" : data.results.name, 
+        ".slot7" : data.results.name, 
+        ".slot8" : data.results.name, 
+        ".slot9" : data.results.name,
+    };
+
+    for(let className in pokeArray) {
+        document.querySelector(className).innerHTML = pokeArray[className];
+    }
+   
+
+};
+*/
+searchBtn.addEventListener("click", function (){
+    displayPokemon(searchBar.value);
+});
+
+searchBar.addEventListener("keydown", function(event){
+    if (event.key === "Enter"){
+        displayPokemon(searchBar.value);
+    }
 });
 
